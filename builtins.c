6d1c168bfef8ff6f69e916a7c932d4c65d1501cd
Rafal Byczek <rafal.algo@gmail.com>
2016-12-03 16:57:48 +0100

    udalo sie poprawic lls

--- builtins.c
+++ builtins.c
@@ -32,21 +32,24 @@ builtin_pair builtins_table[] = {
         {NULL, NULL}
 };
 
-int
-echo(char *argv[]) {
+int echo(char *argv[]) {
+    // no poprostu wypisuje wszystkie argumenty na ekran
     int i = 1;
-    if (argv[i]) printf("%s", argv[i++]);
-    while (argv[i])
-        printf(" %s", argv[i++]);
-
-    printf("\n");
-    fflush(stdout);
+    if (argv[i]) {
+        write(1, argv[i], strlen(argv[i]));
+        i++;
+    }
+    while (argv[i]) {
+        write(1, argv[i], strlen(argv[i]));
+        i++;
+    }
+    write(1, "\n", 1);
     return 0;
 }
 
 
-int
-exitE(char *argv[]) {
+int exitE(char *argv[]) {
+    // exit jak exit
     if (argv[1] != NULL) {
         BUILD_ERROR(argv[0]);
         return BUILTIN_ERROR;
@@ -54,13 +57,11 @@ exitE(char *argv[]) {
     exit(0);
 }
 
-int
-cd(char *argv[]) {
-
-    int ch;
+int cd(char *argv[]) {
+    // robimy komende cd
     if (argv[1] == NULL) {
-        ch = chdir(getenv("HOME"));
-        if (ch == -1) {
+        // w momencie gdy jest samo cd bez argumentow to idziemy do katalogu domowego uzytkownika
+        if (chdir(getenv("HOME")) == -1) {
             BUILD_ERROR(argv[0]);
             return BUILTIN_ERROR;
         }
@@ -68,25 +69,28 @@ cd(char *argv[]) {
     }
 
     if (argv[2] != NULL) {
+        // jak jest cd i miejsce gdzie i jeszcze cos dalej to wywal blad
         BUILD_ERROR(argv[0]);
         return BUILTIN_ERROR;
     }
 
-    ch = chdir(argv[1]);
-    if (ch == -1) {
+    // a jak komenda wyglada na wporzadku to poprostu przechodze do tamtego foldera gdzie mi kazano
+    if (chdir(argv[1]) == -1) {
         BUILD_ERROR(argv[0]);
         return BUILTIN_ERROR;
     }
     return 0;
 }
 
-int
-killK(char *argv[]) {
+int killK(char *argv[]) {
+    // jak komenda ma niepoprawny format to wywalam
     if ((argv[1] == NULL) || (argv[3] != NULL)) {
         BUILD_ERROR(argv[0]);
         return BUILTIN_ERROR;
     }
 
+    // jezeli jest tylko lkill pid i tyle to parsuje ten pid do liczby i idpalam kill z domyslnym
+    // wedle specyfikacji sygnalem sigterm
     if (argv[2] == NULL) {
         int p = 1, pid = 0, i = 0;
         int k = strlen(argv[1]) - 1;
@@ -111,6 +115,7 @@ killK(char *argv[]) {
         return 0;
     }
 
+    // a jak jest podane co to za sygnal to parsuje wszystko co pisze
     int p = 1, pid = 0, signal = 0, i = 0;
     int k = strlen(argv[1]) - 1;
 
@@ -134,7 +139,6 @@ killK(char *argv[]) {
 
     p = 1;
     k = strlen(argv[2]) - 1;
-
     FOR_D(i, k, 0) {
         int c = argv[2][i] - '0';
         if ((c < 0) || (c > 9)) {
@@ -144,7 +148,7 @@ killK(char *argv[]) {
         pid += c * p;
         p *= 10;
     }
-
+    // a jak wszystkow  porzadku sparsuje to wysylam podany sygnal do podanego pidu
     if (kill(pid, signal) == -1) {
         BUILD_ERROR(argv[0]);
         return BUILTIN_ERROR;
@@ -153,13 +157,12 @@ killK(char *argv[]) {
     return 0;
 }
 
-int
-ls(char *argv[]) {
+int ls(char *argv[]) {
     if (argv[1] != NULL) {
         BUILD_ERROR(argv[0]);
         return BUILTIN_ERROR;
     }
-
+    // jak komenda w porzadku podana to otwieram katalog do odpowiedniej struktury
     DIR *dir = opendir(".");
     struct dirent *dp;
 
@@ -168,13 +171,15 @@ ls(char *argv[]) {
         return BUILTIN_ERROR;
     }
 
+    // no i iteruje sie po plikach i wypisuje co trzeba jak nie zaczyna sie nazwa pliku etc od kropki
     while ((dp = readdir(dir))) {
         if (dp == NULL) {
             BUILD_ERROR(argv[0]);
             return BUILTIN_ERROR;
         }
         if (dp->d_name[0] != '.') {
-            fprintf(stdout, "%s\n", dp->d_name);
+            write(1, dp->d_name, strlen(dp->d_name));
+            write(1, "\n", 1);
         }
     }
 
